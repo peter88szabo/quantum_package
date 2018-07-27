92b9515f24d7c87818d50fe78090a47ec2199026
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2018-07-27 11:48:50 +0200

    Removed traceback

--- src/Determinants/Fock_diag.irp.f
+++ src/Determinants/Fock_diag.irp.f
@@ -22,18 +22,18 @@ subroutine build_fock_tmp(fock_diag_tmp,det_ref,Nint)
   if (Ne(1) /= elec_alpha_num) then
     print *,  'Error in build_fock_tmp (alpha)', Ne(1), Ne(2)
     call debug_det(det_ref,N_int)
-    print *,  occ(:,1)
-    print *,  occ(:,2)
-    do i=1,10000
-      occ(i,1) = fock_diag_tmp(1,mo_tot_num+i) !traceback
-    enddo
+!    print *,  occ(:,1)
+!    print *,  occ(:,2)
+!    do i=1,10000
+!      occ(i,1) = fock_diag_tmp(1,mo_tot_num+i) !traceback
+!    enddo
     stop -1
   endif
   if (Ne(2) /= elec_beta_num) then
-    print *, 'Error in build_fock_tmp (beta)', Ne(1), Ne(2)
-    do i=1,10000
-      occ(i,1) = fock_diag_tmp(1,mo_tot_num+i) !traceback
-    enddo
+!    print *, 'Error in build_fock_tmp (beta)', Ne(1), Ne(2)
+!    do i=1,10000
+!      occ(i,1) = fock_diag_tmp(1,mo_tot_num+i) !traceback
+!    enddo
     stop -1
   endif
 
